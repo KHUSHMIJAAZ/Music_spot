4973745d1fbfd0b9a5019c6c3a17566d368907d9
Indranil das <104109233+KHUSHMIJAAZ@users.noreply.github.com>
2023-04-12 23:58:56 +1000

    Add files via upload

---
+++ src/components/sidebar-components/NavList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const NavList = ({children}) => {
+    return (
+        <ul className="nav-list">
+            {children}
+        </ul>
+    );
+}
+
+export default NavList;
